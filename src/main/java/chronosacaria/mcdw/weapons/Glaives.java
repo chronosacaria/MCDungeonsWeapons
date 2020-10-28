04f1e50cd5441e470a5f54d5e8a4a08e9e9bce39
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2020-10-28 03:28:44 -0400

    Texture Updates, New Bow, Breaking Out Weapon Types, Weapons Clean Up, and Lang Files Update Updated the textures for several bows Added the Haunted Bow Broke out weapons as such:      McdwLong and Longs --> McdwSpear, McdwGlaive, McdwStaff, Spears, Glaives, and Staves      Swords --> Swords, Claymores, Rapiers and Curves      Daggers --> Daggers and SoulDaggers Disabled throwing code for Spears Removed McdwLong and Longs Changed weapons types of Spears to Axe, Glaives to Sword, and Staves to Axe Updated Lang files to reflect addition of Haunted Bow

---
+++ src/main/java/chronosacaria/mcdw/weapons/Glaives.java
@@ -0,0 +1,34 @@
+package chronosacaria.mcdw.weapons;
+
+import chronosacaria.mcdw.bases.McdwGlaive;
+import net.minecraft.item.ToolMaterials;
+
+public class Glaives {
+    public static McdwGlaive SPEAR_GLAIVE;
+    public static McdwGlaive SPEAR_VENOM_GLAIVE;
+    public static McdwGlaive SPEAR_GRAVE_BANE;
+
+    public static void init(){
+        SPEAR_GLAIVE =
+                new McdwGlaive(
+                        ToolMaterials.IRON,
+                        3,
+                        -2.7F,
+                        //() -> McdwEntities.SPEAR_GLAIVE,
+                        "spear_glaive");
+        SPEAR_GRAVE_BANE =
+                new McdwGlaive(
+                        ToolMaterials.IRON,
+                        6,
+                        -2.5F,
+                        //() -> McdwEntities.SPEAR_GRAVE_BANE,
+                        "spear_grave_bane");
+        SPEAR_VENOM_GLAIVE =
+                new McdwGlaive(
+                        ToolMaterials.IRON,
+                        5,
+                        -2.5F,
+                        //() -> McdwEntities.SPEAR_VENOM_GLAIVE,
+                        "spear_venom_glaive");
+    }
+}
