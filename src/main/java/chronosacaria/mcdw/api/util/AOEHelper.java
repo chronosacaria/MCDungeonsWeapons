c109109c79e519d494ca8facc2607793462515ba
Fourmisain <8464472+Fourmisain@users.noreply.github.com>
2021-04-24 14:53:38 +0200

    - add getAoeTargets to simplify all AOE methods - remove unused variables

--- src/main/java/chronosacaria/mcdw/api/util/AOEHelper.java
+++ src/main/java/chronosacaria/mcdw/api/util/AOEHelper.java
@@ -17,24 +17,26 @@ import java.util.List;
 
 public class AOEHelper {
 
-    //GRAVITY BEGIN
-    public static void pullInNearbyEntities(LivingEntity user, LivingEntity target, int distance) {
-        World world = target.getEntityWorld();
-        if (!(target instanceof LivingEntity)){
-            return;
-        }
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(target.getBlockPos()).expand(distance),
-            (nearbyEntity) -> nearbyEntity != target && AbilityHelper.isAoeTarget(nearbyEntity, user, target));
-        for (LivingEntity nearbyEntity : nearbyEntities) {
-            if (nearbyEntity == null) return;
-            if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
-            double motionX = target.getX() - (nearbyEntity.getX());
-            double motionY = target.getX() - (nearbyEntity.getY());
-            double motionZ = target.getX() - (nearbyEntity.getZ());
-            Vec3d vec3d = new Vec3d(motionX, motionY, motionZ);
+    /** Returns targets of an AOE effect from 'attacker' around 'center'. This includes 'center'. */
+    public static List<LivingEntity> getAoeTargets(LivingEntity center, LivingEntity attacker, float distance) {
+        return center.getEntityWorld().getEntitiesByClass(LivingEntity.class,
+            new Box(center.getBlockPos()).expand(distance),
+            (nearbyEntity) -> AbilityHelper.isAoeTarget(nearbyEntity, attacker, center)
+        );
+    }
 
-            nearbyEntity.setVelocity(vec3d);
+    //GRAVITY BEGIN
+    public static void pullInNearbyEntities(LivingEntity user, LivingEntity target, float distance) {
+        for (LivingEntity nearbyEntity : getAoeTargets(target, user, distance)) {
+            if (nearbyEntity != target) {
+                if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
+                double motionX = target.getX() - (nearbyEntity.getX());
+                double motionY = target.getX() - (nearbyEntity.getY());
+                double motionZ = target.getX() - (nearbyEntity.getZ());
+                Vec3d vec3d = new Vec3d(motionX, motionY, motionZ);
+
+                nearbyEntity.setVelocity(vec3d);
+            }
         }
     } //GRAVITY END
 
@@ -57,11 +59,7 @@ public class AOEHelper {
     }
 
     public static void electrocuteNearbyEnemies(LivingEntity user, float distance, float damageAmount, int limit){
-        World world = user.getEntityWorld();
-
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(user.getBlockPos()).expand(distance),
-                (nearbyEntity) -> AbilityHelper.isAoeTarget(nearbyEntity, user, user));
+        List<LivingEntity> nearbyEntities = getAoeTargets(user, user, distance);
         if (nearbyEntities.isEmpty()) return;
         if (limit > nearbyEntities.size()) limit = nearbyEntities.size();
         user.world.playSound(null, user.getX(), user.getY(), user.getZ(), SoundEvents.ENTITY_LIGHTNING_BOLT_THUNDER,
@@ -78,17 +76,11 @@ public class AOEHelper {
 
     //EXPLODING BEGIN
     public static void causeExplosionAttack(LivingEntity user, LivingEntity target, float damageAmount, float distance) {
-        World world = target.getEntityWorld();
         DamageSource explosion = DamageSource.explosion(user);
         if (!(target instanceof LivingEntity)){
             return;
         }
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(target.getBlockPos()).expand(distance),
-            (nearbyEntity) -> nearbyEntity != target && AbilityHelper.isAoeTarget(nearbyEntity, user, target));
-        if (nearbyEntities.isEmpty()) return;
-        for (LivingEntity nearbyEntity : nearbyEntities) {
-            if (nearbyEntity == null) return;
+        for (LivingEntity nearbyEntity : getAoeTargets(target, user, distance)) {
             if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
             nearbyEntity.damage(explosion, damageAmount);
         }
@@ -96,93 +88,56 @@ public class AOEHelper {
 
     //CHAINING BEGIN
     public static void chainNearbyEntities(LivingEntity user, LivingEntity target, float distance, int timeMultiplier) {
-        World world = user.getEntityWorld();
-        if (!(target instanceof LivingEntity)){
-            return;
-        }
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(target.getBlockPos()).expand(distance),
-            (nearbyEntity) -> nearbyEntity != target && AbilityHelper.isAoeTarget(nearbyEntity, user, target));
-
-        if (nearbyEntities.isEmpty()) return;
         StatusEffectInstance chained = new StatusEffectInstance(StatusEffects.SLOWNESS, 100 * timeMultiplier, 100);
         target.addStatusEffect(chained);
-        for (LivingEntity nearbyEntity : nearbyEntities) {
-            if (nearbyEntity == null) return;
-            if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
-            double motionX = target.getX() - (nearbyEntity.getX());
-            double motionY = target.getX() - (nearbyEntity.getY());
-            double motionZ = target.getX() - (nearbyEntity.getZ());
-            Vec3d vec3d = new Vec3d(motionX, motionY, motionZ);
+        for (LivingEntity nearbyEntity : getAoeTargets(target, user, distance)) {
+            if (nearbyEntity != target) {
+                if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
+                double motionX = target.getX() - (nearbyEntity.getX());
+                double motionY = target.getX() - (nearbyEntity.getY());
+                double motionZ = target.getX() - (nearbyEntity.getZ());
+                Vec3d vec3d = new Vec3d(motionX, motionY, motionZ);
 
-            nearbyEntity.setVelocity(vec3d);
+                nearbyEntity.setVelocity(vec3d);
 
-            nearbyEntity.addStatusEffect(chained);
+                nearbyEntity.addStatusEffect(chained);
+            }
         }//END CHAINING
     }
 
     public static void causeEchoAttack(LivingEntity user, LivingEntity target, float distance, int echoLevel,
                                        float amount) {
-        World world = target.getEntityWorld();
-        if (!(target instanceof LivingEntity)){
-            return;
-        }
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(target.getBlockPos()).expand(distance),
-            (nearbyEntity) -> nearbyEntity != target && AbilityHelper.isAoeTarget(nearbyEntity, user, target));
-        if (nearbyEntities.isEmpty()) return;
-        for (LivingEntity nearbyEntity : nearbyEntities) {
-            if (nearbyEntity == null) return;
-            if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
-            nearbyEntity.damage(DamageSource.GENERIC, amount);
-            echoLevel--;
-            if (echoLevel <= 0) return;
+        for (LivingEntity nearbyEntity : getAoeTargets(target, user, distance)) {
+            if (nearbyEntity != target) {
+                if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
+                nearbyEntity.damage(DamageSource.GENERIC, amount);
+                echoLevel--;
+                if (echoLevel <= 0) return;
+            }
         }
     }
 
     public static void causeSwirlingAttack(PlayerEntity user, LivingEntity target, float distance, float amount) {
-        World world = target.getEntityWorld();
-        if (!(target instanceof LivingEntity)){
-            return;
-        }
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(user.getBlockPos()).expand(distance),
-            (nearbyEntity) -> nearbyEntity != target && AbilityHelper.isAoeTarget(nearbyEntity, user, user));
-        if (nearbyEntities.isEmpty()) return;
-        for (LivingEntity nearbyEntity : nearbyEntities) {
-            if (nearbyEntity == null) return;
-            if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
-            nearbyEntity.damage(DamageSource.GENERIC, amount * 0.5F);
+        for (LivingEntity nearbyEntity : getAoeTargets(user, user, distance)) {
+            if (nearbyEntity != user) {
+                if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
+                nearbyEntity.damage(DamageSource.GENERIC, amount * 0.5F);
+            }
         }
     }
 
     public static void causeShockwaveAttack(PlayerEntity user, LivingEntity target, float distance, float amount) {
-        World world = target.getEntityWorld();
-        if (!(target instanceof LivingEntity)){
-            return;
-        }
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(target.getBlockPos()).expand(distance),
-            (nearbyEntity) -> nearbyEntity != target && AbilityHelper.isAoeTarget(nearbyEntity, user, target));
-        if (nearbyEntities.isEmpty()) return;
-        for (LivingEntity nearbyEntity : nearbyEntities) {
-            if (nearbyEntity == null) return;
-            if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
-            nearbyEntity.damage(DamageSource.GENERIC, amount * 0.25f);
+        for (LivingEntity nearbyEntity : getAoeTargets(target, user, distance)) {
+            if (nearbyEntity != target) {
+                if (nearbyEntity instanceof PlayerEntity && ((PlayerEntity) nearbyEntity).abilities.creativeMode) return;
+                nearbyEntity.damage(DamageSource.GENERIC, amount * 0.25f);
+            }
         }
     }
 
     public static void causeSmitingAttack(PlayerEntity user, LivingEntity target, float distance, float amount) {
-        World world = target.getEntityWorld();
-        if (!(target instanceof LivingEntity)){
-            return;
-        }
-        List<LivingEntity> nearbyEntities = world.getEntitiesByClass(LivingEntity.class,
-                new Box(target.getBlockPos()).expand(distance),
-            (nearbyEntity) -> nearbyEntity != target && AbilityHelper.isAoeTarget(nearbyEntity, user, target));
-        if (nearbyEntities.isEmpty()) return;
-        for (LivingEntity nearbyEntity : nearbyEntities) {
-            if (nearbyEntity.isUndead()) {
+        for (LivingEntity nearbyEntity : getAoeTargets(target, user, distance)) {
+            if (nearbyEntity != target && nearbyEntity.isUndead()) {
                 nearbyEntity.damage(DamageSource.MAGIC, amount * 1.25F);
             }
         }
