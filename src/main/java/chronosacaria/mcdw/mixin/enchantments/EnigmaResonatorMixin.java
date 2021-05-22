2abcfc20994cdba3e80bee7b98bf8009f7187c02
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-05-22 09:19:07 -0400

    Reworking of EnigmaResonatorMixin to use XP Bar levels

--- src/main/java/chronosacaria/mcdw/mixin/enchantments/EnigmaResonatorMixin.java
+++ src/main/java/chronosacaria/mcdw/mixin/enchantments/EnigmaResonatorMixin.java
@@ -12,17 +12,12 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
 @Mixin(LivingEntity.class)
 public abstract class EnigmaResonatorMixin {
-
-    @Shadow
-    protected abstract int getCurrentExperience(PlayerEntity player);
 
     @Inject(method = "applyDamage(Lnet/minecraft/entity/damage/DamageSource;F)V", at = @At("HEAD"))
     public void applyEnigmaResonatorEnchantmentDamage(DamageSource source, float amount, CallbackInfo info) {
@@ -42,18 +37,13 @@ public abstract class EnigmaResonatorMixin {
                     if (mainHandStack != null && (EnchantmentHelper.getLevel(EnchantsRegistry.ENIGMA_RESONATOR, mainHandStack) >= 1)) {
                         int level = EnchantmentHelper.getLevel(EnchantsRegistry.ENIGMA_RESONATOR, mainHandStack);
 
-                        int numSouls = Math.min((getCurrentExperience((PlayerEntity) user)), 50);
-                        float soulsCriticalBoostChanceCap;
-                        soulsCriticalBoostChanceCap = 0.1F + 0.05F * level;
+                        int numSouls = ((PlayerEntity) source.getAttacker()).experienceLevel;
 
                         float attackDamage = (float) user.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE);
-                        float extraDamageMultiplier = attackDamage == attackDamage ? 1.5F : 3.0F;
+                        float extraDamageMultiplier = (((float)numSouls * level)/30);
                         float getExtraDamage = (attackDamage * (extraDamageMultiplier));
-                        float h = target.getHealth();
 
-                        float chance = user.getRandom().nextFloat();
-                        if (chance <= Math.min((numSouls / 50.0), soulsCriticalBoostChanceCap)) {
-                            //target.setHealth(h - (amount * extraDamageMultiplier));
+                        if (numSouls >= 0 && numSouls <= 100){
 
                             target.damage(DamageSource.GENERIC, getExtraDamage);
                             target.world.playSound(
@@ -61,7 +51,7 @@ public abstract class EnigmaResonatorMixin {
                                     target.getX(),
                                     target.getY(),
                                     target.getZ(),
-                                    SoundEvents.BLOCK_ENDER_CHEST_OPEN,
+                                    SoundEvents.PARTICLE_SOUL_ESCAPE,
                                     SoundCategory.PLAYERS,
                                     0.5F,
                                     1.0F);
