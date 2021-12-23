b63d66be0c2665f7757616953bc8228ea712120b
chronosacaria <67794130+chronosacaria@users.noreply.github.com>
2021-12-23 11:17:44 -0500

    Spears can now use DamageEnchantments  - Spears can now use DamageEnchantments such as Sharpness, Smite, and Bane of Arthropods

---
+++ src/main/java/chronosacaria/mcdw/mixin/DamageEnchantmentMixin.java
@@ -0,0 +1,19 @@
+package chronosacaria.mcdw.mixin;
+
+import chronosacaria.mcdw.bases.McdwSpear;
+import net.minecraft.enchantment.DamageEnchantment;
+import net.minecraft.item.ItemStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(DamageEnchantment.class)
+public class DamageEnchantmentMixin {
+    @Inject(method = "isAcceptableItem", at = @At("HEAD"), cancellable = true)
+    public void enableDamageEnchantmentsForSpears(ItemStack stack, CallbackInfoReturnable<Boolean> cir){
+        if (stack.getItem() instanceof McdwSpear){
+            cir.setReturnValue(true);
+        }
+    }
+}
