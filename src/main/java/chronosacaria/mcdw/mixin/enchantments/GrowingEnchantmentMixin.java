4ff26f590a06b6bd761f42fe0fa8aa63a5fefb48
Kluzzio <kluzzio.amp@gmail.com>
2022-01-29 20:21:53 -0800

    Addressed(#90) Fixed Growing Enchantment  - Growing now mixins to damage in LivingEntity  - Checks were corrected  - Lazy implementation, TODO updated for correcting damage source without crash. Modify Variable is probably the best option for next push.  - Growing now kills the arrow on collision, so it doesn't bounce back and hit the shooter (this does produce a bug with the advancement "Take Aim" but can be fixed later).  - Put in potentially correct template for Modifying Variable to be done later.  - Balancing may happen in future commit, we will see.

--- src/main/java/chronosacaria/mcdw/mixin/enchantments/GrowingEnchantmentMixin.java
+++ src/main/java/chronosacaria/mcdw/mixin/enchantments/GrowingEnchantmentMixin.java
@@ -7,46 +7,75 @@ import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.PersistentProjectileEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(PersistentProjectileEntity.class)
+@Mixin(LivingEntity.class)
 public abstract class GrowingEnchantmentMixin extends Entity {
     public GrowingEnchantmentMixin(EntityType<?> type, World world) {
         super(type, world);
     }
 
-    @Inject(method = "onEntityHit", at = @At("TAIL"))
-    private void onGrowingEnchantmentEntityHit(EntityHitResult entityHitResult, CallbackInfo ci){
-        if (!(entityHitResult.getEntity() instanceof LivingEntity)) {
+    @Shadow
+    public abstract ItemStack getMainHandStack();
+
+    @Inject(method = "damage", at = @At("HEAD"))
+    private void onGrowingEnchantment(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (!((Object) this instanceof LivingEntity livingEntity))
             return;
-        }
-        PersistentProjectileEntity persistentProjectileEntity = (PersistentProjectileEntity) (Object) this;
-        Entity target = entityHitResult.getEntity();
-        LivingEntity shooter = (LivingEntity) persistentProjectileEntity.getOwner();
-        ItemStack mainHandStack = null;
-        if (shooter != null) {
-            mainHandStack = shooter.getMainHandStack();
-        }
-        if (Mcdw.CONFIG.mcdwEnchantmentsConfig.enableEnchantments.get(EnchantmentsID.GROWING)) {
-            if (mainHandStack != null && (EnchantmentHelper.getLevel(EnchantsRegistry.GROWING, mainHandStack) >= 1)) {
-                int level = EnchantmentHelper.getLevel(EnchantsRegistry.GROWING, mainHandStack);
-                double originalDamage = persistentProjectileEntity.getDamage();
-                double damageModifier = 0;
-                if (level == 1) damageModifier = 1.25D;
-                if (level == 2) damageModifier = 1.5D;
-                if (level == 3) damageModifier = 1.75D;
-                double squareDistanceTo = shooter.distanceTo(target);
-                double distance = Math.sqrt(squareDistanceTo);
-                double distanceTraveledModifier = distance * 0.1;
-                persistentProjectileEntity.setDamage(originalDamage * Math.min(distanceTraveledModifier, damageModifier));
+        if (!(source.getSource() instanceof PersistentProjectileEntity persistentProjectileEntity))
+            return;
+        if (!(persistentProjectileEntity.getOwner() instanceof PlayerEntity shooter))
+            return;
+        if (amount > 0 && source.getAttacker() instanceof LivingEntity) {
+            if (Mcdw.CONFIG.mcdwEnchantmentsConfig.enableEnchantments.get(EnchantmentsID.GROWING)) {
+                ItemStack mainHandStack = shooter.getMainHandStack();
+                if (EnchantmentHelper.getLevel(EnchantsRegistry.GROWING, mainHandStack) >= 1) {
+                    int level = EnchantmentHelper.getLevel(EnchantsRegistry.GROWING, mainHandStack);
+
+                    double damageModifier = 1.0D + (0.25D * level);
+                    double distance = shooter.distanceTo(livingEntity);
+                    damageModifier *= distance * 0.1D;
+                    //TODO This mixin can likely be converted to a modify variable (see below) and simply modify amount.
+                    livingEntity.damage(DamageSource.GENERIC, (float) (amount * damageModifier));
+                    livingEntity.world.playSound(
+                            null,
+                            livingEntity.getX(),
+                            livingEntity.getY(),
+                            livingEntity.getZ(),
+                            SoundEvents.ENTITY_ENDERMAN_TELEPORT,
+                            SoundCategory.PLAYERS,
+                            0.5F,
+                            1.0F);
+                    persistentProjectileEntity.remove(RemovalReason.KILLED);
+                }
             }
         }
     }
 }
+/*
+@Mixin(PersistentProjectileEntity.class)
+...
+@ModifyArg(method = "onEntityHit", at = @At(value = "INVOKE", target = "La/b/c/Entity;damage()V"), index = 1)
+private float injected(float amount) {
+
+  // Checks and stuff
+  double damageModifier = 1.0D + (0.25D * level);
+  double distance = shooter.distanceTo(livingEntity);
+  damageModifier *= distance * 0.1D;
+
+  return amount * MathHelper.clamp(damageModifier, 1, 1 + level);
+}
+*/
