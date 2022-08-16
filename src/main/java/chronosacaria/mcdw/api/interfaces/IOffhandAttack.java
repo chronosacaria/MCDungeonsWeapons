7b29c11758506844e2ca5759773dd1e7e867cf12
Kluzzio <kluzzio.amp@gmail.com>
2022-08-16 12:30:50 -0700

    Broom up the == calls for comparing items

--- src/main/java/chronosacaria/mcdw/api/interfaces/IOffhandAttack.java
+++ src/main/java/chronosacaria/mcdw/api/interfaces/IOffhandAttack.java
@@ -3,7 +3,6 @@ package chronosacaria.mcdw.api.interfaces;
 import chronosacaria.mcdw.api.util.PlayerAttackHelper;
 import chronosacaria.mcdw.enums.DaggersID;
 import chronosacaria.mcdw.enums.SicklesID;
-import chronosacaria.mcdw.items.ItemsInit;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
@@ -15,11 +14,11 @@ public interface IOffhandAttack {
     default TypedActionResult<ItemStack> useOffhand(World worldIn, PlayerEntity player, Hand handIn) {
 
 
-        if (handIn == Hand.OFF_HAND && worldIn.isClient && (player.getOffHandStack().getItem() instanceof IOffhandAttack && (player.getOffHandStack().getItem() == player.getMainHandStack().getItem()
-                || (player.getMainHandStack().getItem() == ItemsInit.daggerItems.get(DaggersID.DAGGER_THE_BEGINNING) && player.getOffHandStack().getItem() == ItemsInit.daggerItems.get(DaggersID.DAGGER_THE_END))
-                || (player.getMainHandStack().getItem() == ItemsInit.daggerItems.get(DaggersID.DAGGER_THE_END) && player.getOffHandStack().getItem() == ItemsInit.daggerItems.get(DaggersID.DAGGER_THE_BEGINNING))
-                || (player.getMainHandStack().getItem() == ItemsInit.sickleItems.get(SicklesID.SICKLE_LAST_LAUGH_GOLD) && player.getOffHandStack().getItem() == ItemsInit.sickleItems.get(SicklesID.SICKLE_LAST_LAUGH_SILVER))
-                || (player.getMainHandStack().getItem() == ItemsInit.sickleItems.get(SicklesID.SICKLE_LAST_LAUGH_SILVER) && player.getOffHandStack().getItem() == ItemsInit.sickleItems.get(SicklesID.SICKLE_LAST_LAUGH_GOLD))))) {
+        if (handIn == Hand.OFF_HAND && worldIn.isClient && (player.getOffHandStack().getItem() instanceof IOffhandAttack && (player.getOffHandStack().isOf(player.getMainHandStack().getItem())
+                || (player.getMainHandStack().isOf(DaggersID.DAGGER_THE_BEGINNING.getItem()) && player.getOffHandStack().isOf(DaggersID.DAGGER_THE_END.getItem()))
+                || (player.getMainHandStack().isOf(DaggersID.DAGGER_THE_END.getItem()) && player.getOffHandStack().isOf(DaggersID.DAGGER_THE_BEGINNING.getItem()))
+                || (player.getMainHandStack().isOf(SicklesID.SICKLE_LAST_LAUGH_GOLD.getItem()) && player.getOffHandStack().isOf(SicklesID.SICKLE_LAST_LAUGH_SILVER.getItem()))
+                || (player.getMainHandStack().isOf(SicklesID.SICKLE_LAST_LAUGH_SILVER.getItem()) && player.getOffHandStack().isOf(SicklesID.SICKLE_LAST_LAUGH_GOLD.getItem()))))) {
             PlayerAttackHelper.checkForOffhandAttack();
             ItemStack offhand = player.getStackInHand(handIn);
             return new TypedActionResult<>(ActionResult.SUCCESS, offhand);
