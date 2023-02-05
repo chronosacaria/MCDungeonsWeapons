fd2b10b6a4bc3894e9450739399ece2fc9a1ebf5
Kluzzio <kluzzio.amp@gmail.com>
2023-02-04 16:14:15 -0800

    Fixed #153 - Fixed some issues explicitly - Added framework for fixing crossbow rendering (Thanks DaFuqs and BluStryx8)

---
+++ src/main/java/chronosacaria/mcdw/mixin/client/HeldItemRendererInvoker.java
@@ -0,0 +1,15 @@
+package chronosacaria.mcdw.mixin.client;
+
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.item.HeldItemRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(HeldItemRenderer.class)
+public interface HeldItemRendererInvoker {
+
+    @Invoker("getUsingItemHandRenderType")
+    static HeldItemRenderer.HandRenderType callGetUsingItemHandRenderType(ClientPlayerEntity player) {
+        throw new UnsupportedOperationException();
+    }
+}
