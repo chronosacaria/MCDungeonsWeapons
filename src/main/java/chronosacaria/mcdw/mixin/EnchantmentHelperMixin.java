c840060c9420366903ffc704b854fae9ebc25e38
chronosacaria <chronos.droid@gmail.com>
2023-05-11 12:15:31 -0400

    Manual implementation of https://github.com/chronosacaria/MCDungeonsWeapons/pull/213 because GitHub is being dumb, Version Bump  - Corrected conflicts in EnchantmentHelperMixin  - Updated Mixin Extras  - Version Bump  - Thanks to Daedelus for the pull request. Sorry I couldn't merge it directly :(

--- src/main/java/chronosacaria/mcdw/mixin/EnchantmentHelperMixin.java
+++ src/main/java/chronosacaria/mcdw/mixin/EnchantmentHelperMixin.java
@@ -1,39 +1,55 @@
 package chronosacaria.mcdw.mixin;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
-import net.minecraft.enchantment.EnchantmentTarget;
-import net.minecraft.item.Item;
+import net.minecraft.enchantment.EnchantmentLevelEntry;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.registry.Registry;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
 
-    @Unique
-    private static Enchantment mixedInEnchantment;
+    // This mixin is to fix Vanilla Enchantment table not checking `isAcceptableItem` function
+    @Inject(method = "getPossibleEntries", at = @At("RETURN"))
+    private static void getPossibleEntries_RETURN_SpellEngine(int power, ItemStack stack, boolean treasureAllowed, CallbackInfoReturnable<List<EnchantmentLevelEntry>> cir) {
+        var currentEntries = cir.getReturnValue();
 
-    // Add extra entries from those which were rejected by Vanilla logic
-    @WrapOperation(method = "getPossibleEntries", at = @At(value = "FIELD", target = "Lnet/minecraft/enchantment/Enchantment;type:Lnet/minecraft/enchantment/EnchantmentTarget;"))
-    private static EnchantmentTarget mcdw$getEnchantmentContext(Enchantment enchantment, Operation<EnchantmentTarget> type) {
-        mixedInEnchantment = enchantment;
-        return type.call();
-    }
+        // 1. REMOVING ENCHANT ENTRIES ADDED INCORRECTLY
 
-    @WrapOperation(method = "getPossibleEntries", at = @At(value = "INVOKE", target = "Lnet/minecraft/enchantment/EnchantmentTarget;isAcceptableItem(Lnet/minecraft/item/Item;)Z"))
-    private static boolean mcdw$getPossibleEntries(EnchantmentTarget enchantmentTarget, Item item, Operation<Boolean> returnValue, int power, ItemStack stack, boolean treasureAllowed) {
+        var toRemove = new ArrayList<EnchantmentLevelEntry>();
+        for (var entry: currentEntries) {
+            if (!entry.enchantment.isAcceptableItem(stack)) {
+                toRemove.add(entry);
+            }
+        }
+        currentEntries.removeAll(toRemove);
 
+        // 2. ADDING ENCHANT ENTRIES LEFT OUT INITIALLY
+
+        // This logic is mostly copied from EnchantmentHelper.getPossibleEntries
         boolean isBook = stack.isOf(Items.BOOK);
-        return (!(mixedInEnchantment.isTreasure()
-                && !treasureAllowed
-                || !mixedInEnchantment.isAvailableForRandomSelection()
-                || !mixedInEnchantment.isAcceptableItem(stack) // Custom logic, replacing `!enchantment.type.isAcceptableItem(item)`
-                && !isBook));
+        for (Enchantment enchantment : Registry.ENCHANTMENT) {
+            // Don't check entries already added
+            boolean alreadyAdded = currentEntries.stream().anyMatch(entry -> entry.enchantment.equals(enchantment));
+            if (alreadyAdded) { continue; }
 
+            if (enchantment.isTreasure()
+                    && !treasureAllowed
+                    || !enchantment.isAvailableForRandomSelection()
+                    || !enchantment.isAcceptableItem(stack) // Custom logic, replacing `!enchantment.type.isAcceptableItem(item)`
+                    && !isBook) continue;
+            for (int i = enchantment.getMaxLevel(); i > enchantment.getMinLevel() - 1; --i) {
+                if (power < enchantment.getMinPower(i) || power > enchantment.getMaxPower(i)) continue;
+                currentEntries.add(new EnchantmentLevelEntry(enchantment, i));
+            }
+        }
     }
 }
