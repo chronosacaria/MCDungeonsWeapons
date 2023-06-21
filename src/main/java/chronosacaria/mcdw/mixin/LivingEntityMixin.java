50fa2f940ca109c1be24d8428e6d3cb04ea30ee2
chronosacaria <chronos.droid@gmail.com>
2023-06-21 18:47:47 -0400

    Backported fix for #222, Optimised Imports, Version Bump  - Backported fix for crash associated with Refreshment  - Fixed Gauntlet Recipe

--- src/main/java/chronosacaria/mcdw/mixin/LivingEntityMixin.java
+++ src/main/java/chronosacaria/mcdw/mixin/LivingEntityMixin.java
@@ -29,7 +29,6 @@ import net.minecraft.potion.PotionUtil;
 import net.minecraft.potion.Potions;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.util.Hand;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
