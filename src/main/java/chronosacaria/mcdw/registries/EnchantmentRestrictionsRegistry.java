82f7ba0261b7e4ebc39a9fb58a02a19a3073056b
chronosacaria <chronos.droid@gmail.com>
2023-06-26 13:22:25 -0400

    Added Notes  - Added notes to the EnchantmentRestrictionsRegistry to clarify what each group is doing

--- src/main/java/chronosacaria/mcdw/registries/EnchantmentRestrictionsRegistry.java
+++ src/main/java/chronosacaria/mcdw/registries/EnchantmentRestrictionsRegistry.java
@@ -10,9 +10,14 @@ import net.minecraft.enchantment.Enchantments;
 
 public class EnchantmentRestrictionsRegistry {
     public static void register() {
+        // Individual Enchantment Permissions
         EnchantmentRestriction.permit(Enchantments.FIRE_ASPECT, itemStack -> itemStack.getItem() instanceof McdwAxe || itemStack.getItem() instanceof McdwDoubleAxe);
         EnchantmentRestriction.permit(Enchantments.EFFICIENCY, itemStack -> itemStack.isOf(SwordsID.SWORD_MECHANIZED_SAWBLADE.getItem()));
+
+        // Enchantment Type Permissions
         EnchantmentRestriction.permitTarget((enchantment, itemStack) -> enchantment instanceof DamageEnchantment && itemStack.getItem() instanceof McdwSpear);
+
+        // Individual Enchantment Prohibitions
         EnchantmentRestriction.prohibit(Enchantments.UNBREAKING, itemStack -> itemStack.isOf(SwordsID.SWORD_MECHANIZED_SAWBLADE.getItem()));
         EnchantmentRestriction.prohibit(Enchantments.UNBREAKING, itemStack -> itemStack.isOf(SwordsID.SWORD_BROKEN_SAWBLADE.getItem()));
         EnchantmentRestriction.prohibit(Enchantments.EFFICIENCY, itemStack -> itemStack.isOf(SwordsID.SWORD_BROKEN_SAWBLADE.getItem()));
